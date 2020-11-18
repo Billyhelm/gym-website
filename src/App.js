f1bd33179d6329d1376df7faba4a43080540b745
Billyhelm <billyhelm88@gmail.com>
2020-11-18 13:41:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './default.scss'
+import Header from './components/Header'
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="main">
+      <Homepage />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//currently 13:49 on video #1
+// https://www.youtube.com/watch?v=7gQay39krqY&list=PL-Db3tEF6pB8UO2MmccX-5qeGDX9rek7Q&index=1
